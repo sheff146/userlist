96c05b9452b044062f241a2035d300e4cef036c1
sheff146 <alexandre.shevchenko@gmail.com>
2015-08-29 22:50:17 +0500

    Implemented list of transactions

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@
     var userListApp = angular.module('userListApp', ['ngRoute', 'ngResource']);
 
     userListApp.config([
-        '$routeProvider', '$locationProvider',
-        function ($routeProvider, $locationProvider) {
+        '$routeProvider',
+        function ($routeProvider) {
             $routeProvider
                 .when('/', {
                     templateUrl: 'templates/home.html',
@@ -57,36 +57,41 @@
     ]);
 
     userListApp.controller('UserDetailsCtrl', [
-        '$scope',
-        function ($scope) {
-
+        '$scope', '$location', '$routeParams', 'Transactions',
+        function ($scope, $location, $routeParams, Transactions) {
+            var userId = $routeParams.userId;
+            Transactions.query({userId: userId}, function (data) {
+                $scope.transactions = data;
+            });
         }
     ]);
 
     userListApp.factory('User', [
         '$resource',
         function ($resource) {
-            return $resource(urlPrefix + '/users/', {
+            return $resource(urlPrefix + '/users/:userId', {
                 offset: 0,
                 limit: 10
-                /* http://localhost:8888/phones/phones.json?apiKey=someKeyThis */
             }, {
-                // action: {method: <?>, params: <?>, isArray: <?>, ...}
+                get: {method: 'GET', params: {userId: '@userId'}, isArray: false},
                 update: {method: 'PUT', params: {userId: '@userId'}, isArray: false},
                 create: {method: 'POST', params: {userId: '@userId'}, isArray: false},
-                list: {method: 'GET', params: {offset: '@offset', limit: '@limit'}, isArray: false},
+                list: {method: 'GET', params: {offset: '@offset', limit: '@limit'}, isArray: false}
             });
-            //Phone.update(params, successcb, errorcb);
         }
     ]);
 
-    /*userListApp.controller('ListCtrl', [
-     '$scope', '$http',
-     function ($scope, $http) {
-     $http.get('http://livedemo.xsolla.com/fe/test-task/shevchenko/users?offset=0&limit=10').success(function (data) {
-     $scope.users = data.data;
-     }).error(function () {
-     alert("Error loading users");
-     });
-     }]);*/
+    userListApp.factory('Transactions', [
+        '$resource',
+        function ($resource) {
+            var regex = /\.\d{1,3}Z$/;
+            var timeFromFilter = (new Date(0)).toISOString().replace(regex,'Z');
+            var timeToFilter = (new Date()).toISOString().replace(regex,'Z');
+
+            return $resource(urlPrefix + '/users/:userId/transactions?datetime_from=:timefrom&datetime_to=:timeto', {
+                timefrom: timeFromFilter,
+                timeto: timeToFilter
+            });
+        }
+    ]);
 })();
